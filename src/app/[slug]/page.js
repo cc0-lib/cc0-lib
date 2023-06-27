241f918bebdb2df3d9b60d26b79ce2ed26eed17c
Muhammad Syahman <muhammadsyahman@gmail.com>
2023-06-27 14:05:22 +0800

    fix error and donation address

--- src/app/[slug]/page.js
+++ src/app/[slug]/page.js
@@ -130,7 +130,7 @@ const DetailsPage = ({ params }) => {
 
       {!data && (
         <Link href="/">
-          <h1 className="bg-zinc-800 px-12 py-6 font-chakra text-9xl uppercase text-orange-600 hover:bg-orange-600 hover:text-zinc-800">
+          <h1 className="bg-zinc-800 px-12 py-6 font-chakra text-5xl uppercase text-orange-600 hover:bg-orange-600 hover:text-zinc-800 sm:text-9xl">
             ERROR
           </h1>
         </Link>
