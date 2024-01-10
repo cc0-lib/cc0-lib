5d26ae679a42c6c634dd86b78cee6109bdd68d91
Nero One <info@nero1.net>
2024-01-10 13:10:49 +0800

    feat: testpages, redirect

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,30 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export const middleware = async (req: NextRequest) => {
-  const userToken = req.cookies.get("cc0-lib-siwe")?.value;
+const testPages = ["/draft", "/rive-test", "/loading-test"];
+
+export const middleware = async (request: NextRequest) => {
+  const userToken = request.cookies.get("cc0-lib-siwe")?.value;
 
   if (!userToken) {
-    return NextResponse.redirect(new URL("/dashboard", req.url));
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
+
+  /**
+   * Redirect to the root path if the user is on a test page and not in development mode
+   */
+  if (process.env.NODE_ENV !== "development") {
+    if (testPages.some((page) => request.nextUrl.pathname.startsWith(page))) {
+      NextResponse.rewrite(new URL("/", request.url));
+      return NextResponse.redirect(new URL("/", request.url));
+    }
   }
 };
 
 export const config = {
-  matcher: ["/dashboard/submissions/:path*", "/dashboard/uploader"],
+  matcher: [
+    "/dashboard/submissions/:path*",
+    "/dashboard/uploader",
+    "/",
+    ...testPages,
+  ],
 };
