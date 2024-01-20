3e7d347de909046a58f09350b703fc8a803821e9
Nero One <info@nero1.net>
2024-01-20 10:32:11 +0800

    feat: image-downloader

---
+++ src/components/data/image-dl.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { ArrowDownToLine } from "lucide-react";
+import Link from "next/link";
+import { Route } from "next";
+
+type ImageDownloaderProps = {
+  data: Item;
+};
+
+const ImageDownloader = ({ data }: ImageDownloaderProps) => {
+  return (
+    <>
+      <Link
+        href={data?.Thumbnails[0].url as Route}
+        target="_blank"
+        rel="noreferrer noopener"
+        className="group flex flex-row gap-1 hover:text-prim"
+      >
+        image
+        <ArrowDownToLine
+          className={`h-4 w-4 self-center group-hover:stroke-prim `}
+        />
+      </Link>
+    </>
+  );
+};
+
+export default ImageDownloader;
